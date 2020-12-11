3026026d48354345a164e2ce2ed193564fb6766e
Jay Villari <jay.villari12@gmail.com>
2020-12-11 02:16:22 -0500

    add seed data

--- server.js
+++ server.js
@@ -20,3 +20,20 @@ mongoose.connect(
 db.on("error", (err) => console.log(err.message + " is Mongo not running?"));
 db.on("connected", () => console.log("Mongo connected: ", mongoURI));
 db.on("disconnected", () => console.log("Mongo disconnected"));
+
+// Seed DB
+// const seedWord = {
+//     word: "it",
+//     isChild: "true",
+// };
+  
+// Word.create(seedWord, (error, word) => {
+//     if (error) {
+//       //if there is an error console log it
+//       console.log(error);
+//     } else {
+//       // else show us the created word
+//       console.log(word);
+//     }
+//     db.close();
+//   });
