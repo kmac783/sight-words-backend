dd6499f837099c4e2d1fa29a0d643ae7ab1801c9
Jay Villari <jay.villari12@gmail.com>
2020-12-11 00:52:21 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// Dependencies
+const mongoose = require("mongoose");
+const Word = require("./word.js");
+
+// Global configuration
+const mongoURI = "mongodb://localhost:27017/" + "words";
+const db = mongoose.connection;
+
+// Connect to Mongo
+mongoose.connect(mongoURI);
