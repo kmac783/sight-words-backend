754ef20248a916e0055961a672ae746d91274bb3
Jay Villari <jay.villari12@gmail.com>
2020-12-11 01:18:46 -0500

    clear nodemon errors

--- server.js
+++ server.js
@@ -7,4 +7,10 @@ const mongoURI = "mongodb://localhost:27017/" + "words";
 const db = mongoose.connection;
 
 // Connect to Mongo
-mongoose.connect(mongoURI);
+mongoose.connect(
+    mongoURI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("Connection with Mongo established");
+    }
+  );
